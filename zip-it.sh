af7e62dbd227064b6935fc3cc7530648dc30f17f
Eric Bell <eric@polymorph.co>
2025-06-06 18:48:09 -0400

    added the docker files

---
+++ zip-it.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Usage: ./zip-it.sh preedit|da|sonnet
+
+case "$1" in
+  preedit)
+    ZIPFILE="accessible-outings_preedit.zip"
+    ;;
+  da)
+    ZIPFILE="accessible-outings_DA_postedit.zip"
+    ;;
+  sonnet)
+    ZIPFILE="accessible-outings_Sonnet_postedit.zip"
+    ;;
+  *)
+    echo "Usage: $0 {preedit|da|sonnet}"
+    exit 1
+    ;;
+esac
+
+zip -r "$ZIPFILE" . -x "env/*" "env" ".git/*" ".git" "*/__pycache__/*" "__pycache__" "zip-it.sh" ".env"
